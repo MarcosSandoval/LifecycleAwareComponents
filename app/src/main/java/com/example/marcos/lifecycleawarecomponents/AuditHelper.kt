b1c788d0ebac05a443d2bf3cdc28c9b1c8bfeb59
Marcos Sandoval Calvo <marcos.sandoval@avantica.net>
2018-08-14 17:40:33 -0600

    Documenting code

--- app/src/main/java/com/example/marcos/lifecycleawarecomponents/AuditHelper.kt
+++ app/src/main/java/com/example/marcos/lifecycleawarecomponents/AuditHelper.kt
@@ -5,20 +5,31 @@ package com.example.marcos.lifecycleawarecomponents
  */
 class AuditHelper(private val action1: (String) -> Unit, private val action2: (String) -> Unit) {
 
+    /**
+     * Executed an action when the app starts
+     */
     fun auditStarted(){
         action1.invoke("App started")
     }
 
+    /**
+     * Executed an action when the app is paused
+     */
     fun auditPause(){
         action1.invoke("App paused")
     }
 
+    /**
+     * Executed an action when the app is stopped
+     */
     fun auditStop(){
         action1.invoke("App stopped")
     }
 
+    /**
+     * Reports when any custom action is done
+     */
     fun reportAction(action: String){
         action2.invoke(action)
     }
-
 }
--- app/src/main/java/com/example/marcos/lifecycleawarecomponents/MainActivity.kt
+++ app/src/main/java/com/example/marcos/lifecycleawarecomponents/MainActivity.kt
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        /**
+         * Action that we want to execute when the user start, pause and stop the app.
+         * To keep the example single we are just logging the message, but this can be a request to server,
+         * database operation, etc.
+         * This action is send as parameter to the AuditHelper class
+         */
         var action1: (String) -> Unit = {
             Log.i(TAG, String.format(getString(R.string.report_from_main_activity) , it))
         }
 
+
+        /**
+         * Action to execute if a custom operation is done by the user, in this case we are showing a dialog
+         * but it could be a complex operations
+         * This action is send as parameter to the AuditHelper class
+         */
         var action2: (String) -> Unit = {
             var dialog = AlertDialog.Builder(this)
             with(dialog){
@@ -41,16 +52,25 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onStart() {
+        /**
+         * We report when the app starts
+         */
         auditHelper?.auditStarted()
         super.onStart()
     }
 
     override fun onPause() {
+        /**
+         * Report when the app is paused
+         */
         auditHelper?.auditPause()
         super.onPause()
     }
 
     override fun onStop() {
+        /**
+         * Report when the app is stopped
+         */
         auditHelper?.auditStop()
         super.onStop()
     }
